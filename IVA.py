521c9333e4731a23b523e4502ca5c0f92140d5d5
CristianDalpino <cristiandalpinogs@gmail.com>
2021-09-08 16:25:35 -0300

    Primer commit

---
+++ IVA.py
@@ -0,0 +1,3 @@
+#Programa que permite calcular el IVA de un producto
+#Tendra un sistema de usuario y contrasena (usuario siendo flexible y la contrasena siendo una en especifico)
+#Tambien se podra una cantidad de ese producto si se ingresa mas de 1 cantidad
