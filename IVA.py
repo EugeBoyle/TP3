9549add7cd9d8fcc3c73948f0b07f80b59a99b95
CristianDalpino <cristiandalpinogs@gmail.com>
2021-09-08 16:44:46 -0300

    Algunos retoques (Tercer Commit)

--- IVA.py
+++ IVA.py
@@ -6,7 +6,10 @@ ususario = ''
 contra = 'tremendo'
 prod = ''
 contracheck = ''
+cant = 0
 precio = 0
+ivasumado1prod = 0
+ivaseparado1prod = 0
 ivasumado = 0
 ivaseparado = 0
 
@@ -21,10 +24,18 @@ if contracheck == contra:
 	print('-------------------------------------------------')
 	print('Ingrese los siguientes datos acerca del producto')
 	prod = input("Nombre: ")
-	precio = int(input("Precio: "))
+	cant = int(input("Cantidad: ")) #Ahora puede calcular cantidad!
+	precio = int(input("Precio (C/U): "))
 	ivasumado1prod = precio * 1.21
 	ivaseparado1prod = precio * 0.21
-	print("El producto ", prod, "tiene un IVA de $", ivaseparado1prod, "y sumado al producto, un total de $", ivasumado1prod)
+	ivasumado = (cant * precio) * 1.21
+	ivaseparado = (cant * precio) * 0.21
+	print("El producto ", prod, "tiene individualmente un IVA separado de $", ivaseparado1prod, "y sumado al producto, un total de $", ivasumado1prod)
+	print("En ", cant, " unidades, el IVA separado del producto es de $", ivaseparado, "mientras un total sumado es de $", ivasumado) #
 	print('-------------------------------------------------')
+	input("Presione cualquier tecla para salir")#Para evitar que el programa se cierre al instante
 else:
+	print('-------------------------------------------------')
 	print('Contrasena incorrecta')
+	print('-------------------------------------------------')
+	input("Presione cualquier tecla para salir")
